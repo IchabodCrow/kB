e8a69db9902a99f105c10a1cd4e711ede9442593
ichabod <mrdoy@mail.ru>
2019-11-26 18:48:49 +0300

    newLink

--- src/index.js
+++ src/index.js
@@ -3,34 +3,23 @@
   2)  Навешиваем хендлера на ссылки и вместо перехода по ссылке подставляем нужный темпл
 */
 
+const link = $('.link_left_bar');
 
+$('body').on('click', '.link_left_bar', (event) => {
 
-function index() {
-  const indexTmpl = `#${location.href.split('/').pop().split('.')[0]}Tmpl`;
-  $('#centralBlock').html($(indexTmpl).tmpl());
-};
-index();
+if (event.target.dataset.id === '0') {
+    const indexTmpl = `#${location.href.split('/').pop().split('.')[0]}Tmpl`;
+    $('#centralBlock').html($(indexTmpl).tmpl());
+    event.preventDefault();
 
-function burbon() {
-  const burbonTmpl = `#${burbonLnk[0].href.split('/').pop().split('.')[0]}Tmpl`;
-  $('#centralBlock').html($(burbonTmpl).tmpl());
-};
+} if (event.target.dataset.id === '2') {
+    const burbonTmpl = `#${link[2].href.split('/').pop().split('.')[0]}Tmpl`;
+    $('#centralBlock').html($(burbonTmpl).tmpl());
+    event.preventDefault();
 
-function teqiula() {
-  const teqiulaTmpl = `#${teqiulaLnk[0].href.split('/').pop().split('.')[0]}Tmpl`;
-  $('#centralBlock').html($(teqiulaTmpl).tmpl());
-};
-
-const burbonLnk = $('.linkBurbon').on('click', function(event) {
-  const href = $(this).attr('href');
-  console.log(href);
-});
-
-const teqiulaLnk = $('.linkTeqiula').on('click', function(event) {
- const href = $(this).attr('href');
- console.log(href);
-});
-
-$('body').on('click','.linkIndex', () => index(event.preventDefault()));
-$('body').on('click','.linkBurbon', () => burbon(event.preventDefault()));
-$('body').on('click','.linkTeqiula', () => teqiula(event.preventDefault()));
+} if (event.target.dataset.id === '6'){
+    const teqiulaTmpl = `#${link[6].href.split('/').pop().split('.')[0]}Tmpl`;
+    $('#centralBlock').html($(teqiulaTmpl).tmpl());
+    event.preventDefault();
+}
+})
