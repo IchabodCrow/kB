83d35e5c72fe821ebd530004733d585610c89e8f
ichabod <mrdoy@mail.ru>
2019-11-26 19:16:37 +0300

    href

--- src/index.js
+++ src/index.js
@@ -6,20 +6,21 @@
 const link = $('.link_left_bar');
 
 $('body').on('click', '.link_left_bar', (event) => {
+console.log(event.target.href);
+if (event.target.href == "file:///C:/Users/play-/apps/kb/src/index.html") {
+  const indexTmpl = `#${location.href.split('/').pop().split('.')[0]}Tmpl`;
+  $('#centralBlock').html($(indexTmpl).tmpl());
+  event.preventDefault();
+}
 
-if (event.target.dataset.id === '0') {
-    const indexTmpl = `#${location.href.split('/').pop().split('.')[0]}Tmpl`;
-    $('#centralBlock').html($(indexTmpl).tmpl());
-    event.preventDefault();
+if (event.target.href == 'file:///C:/Users/play-/apps/kb/src/burbon.html') {
+  const burbonTmpl = `#${link[2].href.split('/').pop().split('.')[0]}Tmpl`;
+  $('#centralBlock').html($(burbonTmpl).tmpl());
+  event.preventDefault();
 
-} if (event.target.dataset.id === '2') {
-    const burbonTmpl = `#${link[2].href.split('/').pop().split('.')[0]}Tmpl`;
-    $('#centralBlock').html($(burbonTmpl).tmpl());
-    event.preventDefault();
-
-} if (event.target.dataset.id === '6'){
-    const teqiulaTmpl = `#${link[6].href.split('/').pop().split('.')[0]}Tmpl`;
-    $('#centralBlock').html($(teqiulaTmpl).tmpl());
-    event.preventDefault();
+} if (event.target.href == 'file:///C:/Users/play-/apps/kb/src/teqiula.html'){
+  const teqiulaTmpl = `#${link[6].href.split('/').pop().split('.')[0]}Tmpl`;
+  $('#centralBlock').html($(teqiulaTmpl).tmpl());
+  event.preventDefault();
 }
 })
